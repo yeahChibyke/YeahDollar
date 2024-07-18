a77175de66c1ca865402373e7a7ae9fe18ceac76
yeahChibyke <chibykepride@gmail.com>
2024-07-18 12:42:23 +0100

    finished with YeahDollar contract

---
+++ src/YeahDollar.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity >= 0.8.0;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @title YeahDollar StableCoin
+/// @author Chukwubuike Victory Chime
+/// @notice This contract is just the ERC20 implementation of the stablecoin, and it will be governed by the YeahDollarEngine
+/// @dev Relative Stability: Pegged to USD
+/// @dev Stablity Mechanism: Algorithmic
+/// @dev Collateral: Exogenous (wETH and wBTC)
+contract YeahDollar is ERC20Burnable, Ownable {
+    // >------< Errors >------<
+    error YeahDollar__AmountMustBeMoreThanZero();
+    error YeahDollar__BurnAmountExceedsBalance();
+    error YeahDollar__MustNotBeZeroAddress();
+
+    // >------< Constructor >-----<
+    constructor() ERC20("YeahDollar", "Y$") Ownable(0x25571828c3F5cdC6f57d124dccd65D1be7ECaA2e) {}
+
+    // >------< Functions >------<
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+
+        if (_amount <= 0) {
+            revert YeahDollar__AmountMustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert YeahDollar__BurnAmountExceedsBalance();
+        }
+
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert YeahDollar__MustNotBeZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert YeahDollar__AmountMustBeMoreThanZero();
+        }
+
+        _mint(_to, _amount);
+        return true;
+    }
+}

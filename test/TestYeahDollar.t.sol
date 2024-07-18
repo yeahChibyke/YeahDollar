91595c6cc274dfcbfd2d998c6c6d8993502f136a
yeahChibyke <chibykepride@gmail.com>
2024-07-18 09:22:37 +0100

    contracts check 1 2

---
+++ test/TestYeahDollar.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity >= 0.8.0;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {YeahDollar} from "../src/YeahDollar.sol";
+
+contract TestYeahDollar is Test {}

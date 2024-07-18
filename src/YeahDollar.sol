fc246ea2703f8581c9bcadbd221a69354f8868b7
yeahChibyke <chibykepride@gmail.com>
2024-07-18 15:14:37 +0100

    edited comments

--- src/YeahDollar.sol
+++ src/YeahDollar.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity >= 0.8.0;
 
-// >------< Imports >-----<
+// ---------------------------< IMPORTS >------------------------------------------------------------------------------------------------------------------------------>>>
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -17,10 +17,10 @@ contract YeahDollar is ERC20Burnable, Ownable {
     error YeahDollar__BurnAmountExceedsBalance();
     error YeahDollar__MustNotBeZeroAddress();
 
-    // >------< Constructor >-----<
+    // ---------------------------< CONSTRUCTOR >------------------------------------------------------------------------------------------------------------------------------>>>
     constructor() ERC20("YeahDollar", "Y$") Ownable(0x25571828c3F5cdC6f57d124dccd65D1be7ECaA2e) {}
 
-    // >------< Functions >------<
+    // ---------------------------< FUNCTIONS >------------------------------------------------------------------------------------------------------------------------------>>>
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
 

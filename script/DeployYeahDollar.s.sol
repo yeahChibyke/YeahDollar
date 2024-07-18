a530ac09f89bb48545f94d69a380efdbcfe5132b
yeahChibyke <chibykepride@gmail.com>
2024-07-18 09:24:40 +0100

    chore: forge init

--- script/DeployYeahDollar.s.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity >= 0.8.0;
-
-import {Script} from "forge-std/Script.sol";
-import {YeahDollar} from "../src/YeahDollar.sol";
-
-contract DeployYeahDollar is Script {}

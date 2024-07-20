c058066f696ab4bc57ca861fda7696d83ff2f1b7
yeahChibyke <chibykepride@gmail.com>
2024-07-20 14:07:34 +0100

    done with DeployScript

--- script/DeployYeahDollar.s.sol
+++ script/DeployYeahDollar.s.sol
@@ -4,12 +4,28 @@ pragma solidity >=0.6.2 <0.9.0;
 
 import {Script} from "forge-std/Script.sol";
 import {YeahDollar} from "../src/YeahDollar.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {YeahDollarEngine} from "../src/YeahDollarEngine.sol";
 
 contract DeployYeahDollar is Script {
-    function run() external returns (YeahDollar) {
-        vm.startBroadcast();
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+    function run() external returns (YeahDollar, YeahDollarEngine) {
+        HelperConfig helperConfig = new HelperConfig();
+
+        (address wEthUsdPriceFeed, address wBtcUsdPriceFeed, address wEth, address wBtc, uint256 deployerKey) =
+            helperConfig.activeNetworkConfig();
+
+        tokenAddresses = [wEth, wBtc];
+        priceFeedAddresses = [wEthUsdPriceFeed, wBtcUsdPriceFeed];
+
+        vm.startBroadcast(deployerKey);
         YeahDollar yeahDollar = new YeahDollar();
+        YeahDollarEngine yeahDollarEngine =
+            new YeahDollarEngine(tokenAddresses, priceFeedAddresses, address(yeahDollar));
+        yeahDollar.transferOwnership(address(yeahDollarEngine));
         vm.stopBroadcast();
-        return yeahDollar;
+        return (yeahDollar, yeahDollarEngine);
     }
 }

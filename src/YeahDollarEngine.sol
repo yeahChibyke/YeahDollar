2ff88db713506ed0f0570d0df1bab0c336bf677f
yeahChibyke <chibykepride@gmail.com>
2024-07-25 20:37:59 +0100

    a lot of refactorings. pheww!

--- src/YeahDollarEngine.sol
+++ src/YeahDollarEngine.sol
@@ -125,14 +125,14 @@ contract YeahDollarEngine is ReentrancyGuard {
      * @param tokenCollateralAddress Address of the token of the collateral to be redeemed
      * @param amountCollateral Amount of collateral to be redeemed
      * @param amountYDToBurn Amount of YD to be burnt
-     * @notice This function will burn YD and redeem underlying collateral in one transaction
-     * @dev redeemCollateral() function already has _revertIfHealthFactorIsBroken(), so no to put it
+     * @notice This function will burn YD and redeem/withdraw underlying collateral in one transaction
      */
     function redeemCollateralForYD(address tokenCollateralAddress, uint256 amountCollateral, uint256 amountYDToBurn)
         external
     {
-        burnYD(amountYDToBurn);
-        redeemCollateral(tokenCollateralAddress, amountCollateral);
+        _burnYD(amountYDToBurn, msg.sender, msg.sender);
+        _redeemCollateral(tokenCollateralAddress, amountCollateral, msg.sender, msg.sender);
+        _revertIfHealthFactorIsBroken(msg.sender);
     }
 
     /**
@@ -149,6 +149,15 @@ contract YeahDollarEngine is ReentrancyGuard {
         _redeemCollateral(tokenCollateralAddress, amountCollateral, msg.sender, msg.sender);
 
         _revertIfHealthFactorIsBroken(msg.sender);
+    }
+
+    /**
+     * @param amountYDToBurn Amount of YD to be burnt
+     * @notice This function will burn YD when called, only call if you are sure
+     */
+    function burnYD(uint256 amountYDToBurn) public shouldBeMoreThanZero(amountYDToBurn) {
+        _burnYD(amountYDToBurn, msg.sender, msg.sender);
+        _revertIfHealthFactorIsBroken(msg.sender); // likelihood of this happening is very very unlikely
     }
 
     /**
@@ -173,7 +182,7 @@ contract YeahDollarEngine is ReentrancyGuard {
         }
 
         // We need to know the USD equivalent of debt to be covered.
-        //      E.g. If covering D100 of YD, we need to know what the ETH equivalent of that debt is
+        //      E.g. If covering 100 of YD, we need to know what the ETH equivalent of that debt is
         uint256 tokenAmountFromDebtCovered = getTokenAmountFromUsd(collateralAddress, debtToCover);
 
         // We also want to give liquidators a 10% bonus
@@ -186,6 +195,7 @@ contract YeahDollarEngine is ReentrancyGuard {
         _burnYD(debtToCover, user, msg.sender);
 
         uint256 finalUserHealthFactor = _healthFactor(user);
+        // This condition should never hit, but no harm in being too careful
         if (finalUserHealthFactor <= initialUserHealthFactor) {
             revert YeahDollarEngine__HealthFactorNotIproved();
         }
@@ -224,7 +234,12 @@ contract YeahDollarEngine is ReentrancyGuard {
      * @notice This function will mint YD when called
      * @notice Minting will fail if collateral value > minimum threshold
      */
-    function mintYD(uint256 amountYDToMint, address tokenCollateralAddress) public shouldBeMoreThanZero(amountYDToMint) nonReentrant mintAmountMustBeLessOrEqualToUserDepositCollateral(amountYDToMint, tokenCollateralAddress) {
+    function mintYD(uint256 amountYDToMint, address tokenCollateralAddress)
+        public
+        shouldBeMoreThanZero(amountYDToMint)
+        nonReentrant
+        mintAmountMustBeLessOrEqualToUserDepositCollateral(amountYDToMint, tokenCollateralAddress)
+    {
         s_YDMinted[msg.sender] += amountYDToMint;
 
         _revertIfHealthFactorIsBroken(msg.sender);
@@ -236,82 +251,9 @@ contract YeahDollarEngine is ReentrancyGuard {
         }
     }
 
-    /**
-     * @param amountYDToBurn Amount of YD to be burnt
-     * @notice This function will burn YD when called
-     */
-    function burnYD(uint256 amountYDToBurn) public shouldBeMoreThanZero(amountYDToBurn) {
-        _burnYD(amountYDToBurn, msg.sender, msg.sender);
-
-        i_yd.burn(amountYDToBurn);
-
-        _revertIfHealthFactorIsBroken(msg.sender); // likelihood of this happening is very very unlikely
-    }
-
-    function getAccountCollateralValue(address user) public view returns (uint256 totalCollateralValueInUsd) {
-        for (uint256 i = 0; i < s_collateralTokens.length; i++) {
-            address token = s_collateralTokens[i];
-            uint256 amount = s_collateralDeposited[user][token];
-            totalCollateralValueInUsd += getUsdValue(token, amount);
-        }
-        return totalCollateralValueInUsd;
-    }
-
-    function getUsdValue(address token, uint256 amount) public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
-        (, int256 answer,,,) = priceFeed.latestRoundData();
-        return ((uint256(answer) * ADDITIONAL_FEED_PRECISION) * amount) / PRECISION;
-    }
-
     // >------------------------------------------------------------------------------------------------------------------------------>>>
 
-    // >------< PUBLIC & EXTERNAL VIEW FUNCTIONS >-----<
-
-    function getTokenAmountFromUsd(address token, uint256 usdAmountInWei) public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
-        (, int256 answer,,,) = priceFeed.latestRoundData();
-        return (usdAmountInWei * PRECISION) / (uint256(answer) * ADDITIONAL_FEED_PRECISION);
-    }
-
-    function getAccountInformation(address user)
-        external
-        view
-        returns (uint256 totalYDMinted, uint256 collateralValueInUsd)
-    {
-        (totalYDMinted, collateralValueInUsd) = _getAccountInformation(user);
-    }
-
-    function gethealthFactor() external view {}
-
-    // >------------------------------------------------------------------------------------------------------------------------------>>>
-
-    // >------< PRIVATE & INTERNAL VIEW FUNCTIONS >-----<
-    function _getAccountInformation(address user)
-        private
-        view
-        returns (uint256 totalYDMinted, uint256 collateralValueInUsd)
-    {
-        totalYDMinted = s_YDMinted[user];
-        collateralValueInUsd = getAccountCollateralValue(user);
-    }
-
-    /**
-     * Returns how close to liquidation a user is
-     * If a user goes below 1, they are at risk of getting liquidated
-     */
-    function _healthFactor(address user) private view returns (uint256) {
-        (uint256 totalYDMinted, uint256 collateralValueInUsd) = _getAccountInformation(user);
-        uint256 collateralAdjustedForThreshold = (collateralValueInUsd * LIQUIDATION_THRESHOLD) / LIQUIDATION_PRECISION;
-        return (collateralAdjustedForThreshold * PRECISION) / totalYDMinted;
-    }
-
-    function _revertIfHealthFactorIsBroken(address user) internal view {
-        uint256 userHealthFactor = _healthFactor(user);
-
-        if (userHealthFactor < MIN_HEALTH_FACTOR) {
-            revert YeahDollarEngine__HealthFactorIsBroken(userHealthFactor);
-        }
-    }
+    // >------< PRIVATE FUNCTIONS >-----<
 
     function _redeemCollateral(address tokenCollateralAddress, uint256 amountCollateral, address from, address to)
         private
@@ -334,5 +276,132 @@ contract YeahDollarEngine is ReentrancyGuard {
         }
 
         i_yd.burn(amountYDToBurn);
+    }
+
+    // >------------------------------------------------------------------------------------------------------------------------------>>>
+
+    // >------< PRIVATE & INTERNAL VIEW FUNCTIONS >-----<
+
+    function _getAccountInformation(address user)
+        private
+        view
+        returns (uint256 totalYDMinted, uint256 collateralValueInUsd)
+    {
+        totalYDMinted = s_YDMinted[user];
+        collateralValueInUsd = getAccountCollateralValue(user);
+    }
+
+    function _healthFactor(address user) private view returns (uint256) {
+        (uint256 totalYDMinted, uint256 collateralValueInUsd) = _getAccountInformation(user);
+        return _calculateHealthFactor(totalYDMinted, collateralValueInUsd);
+    }
+
+    function _getUsdValue(address token, uint256 amount) private view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
+        (, int256 answer,,,) = priceFeed.latestRoundData();
+        // Most USD pairs have 8 decimals, so we will assume they all do
+        // We want to have everything in terms of wei, so we add 10 zeros at the end
+        return ((uint256(answer) * ADDITIONAL_FEED_PRECISION) * amount) / PRECISION;
+    }
+
+    function _calculateHealthFactor(uint256 totalYDMinted, uint256 collateralValueInUsd)
+        internal
+        pure
+        returns (uint256)
+    {
+        if (totalYDMinted == 0) return type(uint256).max;
+        uint256 collateralAdjustedForThreshold = (collateralValueInUsd * LIQUIDATION_THRESHOLD) / LIQUIDATION_PRECISION;
+        return (collateralAdjustedForThreshold * PRECISION) / totalYDMinted;
+    }
+
+    function _revertIfHealthFactorIsBroken(address user) internal view {
+        uint256 userHealthFactor = _healthFactor(user);
+
+        if (userHealthFactor < MIN_HEALTH_FACTOR) {
+            revert YeahDollarEngine__HealthFactorIsBroken(userHealthFactor);
+        }
+    }
+
+    // >------------------------------------------------------------------------------------------------------------------------------>>>
+
+    // >------< PUBLIC & EXTERNAL VIEW & PURE FUNCTIONS >-----<
+
+    function calculateHealthFactor(uint256 totalYDMinted, uint256 collateralValueInUsd)
+        external
+        pure
+        returns (uint256)
+    {
+        return _calculateHealthFactor(totalYDMinted, collateralValueInUsd);
+    }
+
+    function getTokenAmountFromUsd(address token, uint256 usdAmountInWei) public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
+        (, int256 answer,,,) = priceFeed.latestRoundData();
+        return (usdAmountInWei * PRECISION) / (uint256(answer) * ADDITIONAL_FEED_PRECISION);
+    }
+
+    function getAccountInformation(address user)
+        external
+        view
+        returns (uint256 totalYDMinted, uint256 collateralValueInUsd)
+    {
+        (totalYDMinted, collateralValueInUsd) = _getAccountInformation(user);
+    }
+
+    function getAccountCollateralValue(address user) public view returns (uint256 totalCollateralValueInUsd) {
+        for (uint256 i = 0; i < s_collateralTokens.length; i++) {
+            address token = s_collateralTokens[i];
+            uint256 amount = s_collateralDeposited[user][token];
+            totalCollateralValueInUsd += _getUsdValue(token, amount);
+        }
+        return totalCollateralValueInUsd;
+    }
+
+    function getUsdValue(address token, uint256 amountInWei) public view returns (uint256) {
+        return _getUsdValue(token, amountInWei);
+    }
+
+    function getCollateralBalanceOfUser(address user, address token) external view returns (uint256) {
+        return s_collateralDeposited[user][token];
+    }
+
+    function getPrecision() external pure returns (uint256) {
+        return PRECISION;
+    }
+
+    function getAdditionalFeedPrecision() external pure returns (uint256) {
+        return ADDITIONAL_FEED_PRECISION;
+    }
+
+    function getLiquidationThreshold() external pure returns (uint256) {
+        return LIQUIDATION_THRESHOLD;
+    }
+
+    function getLiquidationBonus() external view returns (uint256) {
+        return LIQUIDATION_BONUS;
+    }
+
+    function getLiquidationPrecision() external pure returns (uint256) {
+        return LIQUIDATION_PRECISION;
+    }
+
+    function getMinHealthFactor() external pure returns (uint256) {
+        return MIN_HEALTH_FACTOR;
+    }
+
+    function getCollateralTokens() external view returns (address[] memory) {
+        return s_collateralTokens;
+    }
+
+    function getYD() external view returns (address) {
+        return address(i_yd);
+    }
+
+    function getCollateralTokenPriceFeed(address token) external view returns (address) {
+        return s_priceFeeds[token];
+    }
+
+    function gethealthFactor(address user) external view returns(uint256) {
+        return _healthFactor(user);
     }
 }
